76d808402846414240964fbb395b1767b029850a
Alisha Khatri <alisha.191502@ncit.edu.np>
2024-06-14 17:10:02 +0545

    responsivee

--- src/app/bansaj-created/page.tsx
+++ src/app/bansaj-created/page.tsx
@@ -8,18 +8,18 @@ export default function Page() {
       style={{ backgroundImage: 'url("/leafs/fullleaf.svg")' }}
     >
       <div className="flex flex-col items-center space-y-8 ">
-        <div className="bg-[#0051BA] bg-opacity-90 rounded-full lg:p-6 w-[300px] md:w-[500px] md:flex-row md:p-4 lg:w-[600px] flex-col lg:flex-row  p-2 flex justify-between items-center text-white  lg:space-x-32">
+        <div className="bg-[#0051BA] bg-opacity-90 rounded-full lg:p-6 w-[300px] md:w-[500px] md:flex-row md:p-4 lg:w-[600px] flex-col lg:flex-row  p-2 flex justify-between items-center text-white  ">
           <h1 className="text-lg">
             Bansaj Created:{" "}
             <span className="lg:text-2xl text-xl font-semibold">200</span>
           </h1>
           <h1 className="text-lg">
-            Bansaj Created Today:{" "}
+            Bansaj Created Today: {""}
             <span className="lg:text-2xl text-xl font-semibold">10</span>
           </h1>
         </div>
-        <div className="bg-[#0051BA] bg-opacity-90 rounded-full lg:p-5 p-3 lg:w-[850px] md:w-[700px]  w-[350px] flex justify-around items-center text-white text-xl lg:space-x-64 md:space-x-32 space-x-6">
-          <div className="flex flex-col items-center space-y-1  ml-0">
+        <div className="bg-[#0051BA] bg-opacity-90 rounded-full lg:p-5 p-3 xl:w-[850px] lg:w-[800px] md:w-[700px]  w-[350px] sm:w-[500px] flex justify-between items-center text-white text-xl">
+          <div className="flex flex-col items-center space-y-1 ">
             <div className="flex items-center space-x-2">
               <Cake size={40} />
               <h1 className="font-semibold  lg:text-2xl text-xl">200</h1>
